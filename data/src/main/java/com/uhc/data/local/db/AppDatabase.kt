dd19855cf7b1b7d1565dc20f25f526a99c8b1b47
Uryel <uryelhenrique.c@gmail.com>
2019-11-16 10:16:01 +0100

    Create app database

---
+++ data/src/main/java/com/uhc/data/local/db/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.uhc.data.local.db
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.uhc.data.R
+import com.uhc.data.local.db.client.JokeDao
+import com.uhc.data.local.db.client.JokeEntity
+
+/**
+ * Created by Constancio on 2019-11-16.
+ */
+@Database(entities = [JokeEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        fun createDatabase(application: Application): AppDatabase {
+            return Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                application.getString(R.string.database_name)
+            ).allowMainThreadQueries().build()
+        }
+    }
+
+    abstract fun jokeDao(): JokeDao
+}
