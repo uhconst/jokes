7ec14735d61b3533ed2cc374c79b1c999897c2be
Uryel <uryelhenrique.c@gmail.com>
2019-11-17 23:16:22 +0300

    Remove unused main view model

--- presentation/src/main/java/com/uhc/presentation/MainActivity.kt
+++ presentation/src/main/java/com/uhc/presentation/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.uhc.presentation
 
-import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import com.uhc.presentation.databinding.MainActivityBinding
 import com.uhc.presentation.ui.base.BaseActivity
-import org.koin.android.viewmodel.ext.android.viewModel
 
 /**
  * Created by Constancio on 2019-11-16.
  */
 class MainActivity : BaseActivity<MainActivityBinding>() {
-    val viewModel: MainViewModel by viewModel()
-
     override fun getLayoutRes(): Int = R.layout.main_activity
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.viewModel = viewModel
-    }
 
     fun setupTollbar(toolbar: Toolbar, enableBackButton: Boolean) {
         setSupportActionBar(toolbar)
