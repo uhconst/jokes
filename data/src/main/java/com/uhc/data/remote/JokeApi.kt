e8f68c4dd84ef3dff074f55a90ae31920ec63aa7
Uryel <uryelhenrique.c@gmail.com>
2019-11-19 19:09:53 +0300

    Change parameters name

--- data/src/main/java/com/uhc/data/remote/JokeApi.kt
+++ data/src/main/java/com/uhc/data/remote/JokeApi.kt
@@ -9,6 +9,6 @@ import retrofit2.http.Path
  * Created by Constancio on 2019-11-16.
  */
 interface JokeApi {
-    @GET("random/{jokesQuantity/") // TODO: Change random value following doc
-    fun getJokes(@Path("jokesQuantity") jokeQuantity: Int):  Single<ResponseWrap<JokeDto.Response>>
+    @GET("random/{limit}")
+    fun getJokes(@Path("limit") limit: Int):  Single<ResponseWrap<JokeDto.Response>>
 }
