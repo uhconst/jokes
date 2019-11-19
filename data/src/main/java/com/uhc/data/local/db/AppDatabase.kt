17117532cd3d9c9bf046bf06e3cd780b61b2472d
Uryel <uryelhenrique.c@gmail.com>
2019-11-19 13:49:14 +0300

    Refactor after package rename

--- data/src/main/java/com/uhc/data/local/db/AppDatabase.kt
+++ data/src/main/java/com/uhc/data/local/db/AppDatabase.kt
@@ -5,8 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.uhc.data.R
-import com.uhc.data.local.db.client.JokeDao
-import com.uhc.data.local.db.client.JokeEntity
+import com.uhc.data.local.db.joke.JokeDao
+import com.uhc.data.local.db.joke.JokeEntity
 
 /**
  * Created by Constancio on 2019-11-16.
