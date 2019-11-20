b19e02cb77f0097437dec6c11a56559911823681
Uryel <uryelhenrique.c@gmail.com>
2019-11-20 10:10:52 +0300

    Fix remote load and local load

--- presentation/src/main/java/com/uhc/presentation/joke/JokesListViewModel.kt
+++ presentation/src/main/java/com/uhc/presentation/joke/JokesListViewModel.kt
@@ -11,7 +11,7 @@ import com.uhc.presentation.utils.EventLiveData
  * Created by Constancio on 2019-11-17.
  */
 class JokesListViewModel(
-    jokeRepo: JokeRepository
+    private val jokeRepo: JokeRepository
 ) : BaseViewModel() {
     private val _jokes = MutableLiveData<List<Joke>>()
     val jokes: LiveData<List<Joke>> get() = _jokes
@@ -20,13 +20,11 @@ class JokesListViewModel(
     val events: LiveData<JokeListEvents> get() = _events
 
     init {
-        subscribeCompletable(
-            observable = jokeRepo.loadRandomJokes(5) // TODO: Random value
-                .doOnSubscribe { startProgress() }
-                .doFinally { stopProgress() },
-            error = { _events.postValue(JokeListEvents.JOKES_ERROR) }
-        )
+        jokeRepo.loadRemoteRandomJokes(5)// TODO: Random value
+        getJokes()
+    }
 
+    private fun getJokes() {
         subscribeObservable(
             observable = jokeRepo.observeJokes(5)// TODO: Random value
                 .doOnSubscribe { startProgress() }
